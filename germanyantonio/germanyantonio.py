6cc56c6d188bea3718efb606157b0451248f410b
ajcasadob <ajcasbay@gmail.com>
2025-10-24 14:30:45 +0200

    done

---
+++ germanyantonio/germanyantonio.py
@@ -0,0 +1,18 @@
+provincias  = ['Sevilla','Cadiz','Malaga','Granada','Huelva','Jaen','Cordoba','Almeria']
+provincias.remove('Jaen')
+
+print(provincias)
+
+provincias.sort()
+print(provincias)
+
+provincias.insert(0,"Malaga")
+print(provincias)
+
+del provincias [3:5]
+print(provincias)
+
+
+
+print(provincias)
+
