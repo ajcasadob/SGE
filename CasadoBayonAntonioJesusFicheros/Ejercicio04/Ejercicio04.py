f2db5f9ed6d93f0970cc59b63d164976d21f0d9b
Antonio Jesús <ajcasbay@gmail.com>
2025-11-23 13:49:38 +0100

    done

---
+++ CasadoBayonAntonioJesusFicheros/Ejercicio04/Ejercicio04.py
@@ -0,0 +1,17 @@
+#Crea un programa que lea un fichero llamado 'texto.txt' y genere 'numerado.txt' con todas las líneas
+#precedidas de su número.
+
+
+archivo_entrada = open('texto.txt', 'r')
+lineas = archivo_entrada.readlines()
+archivo_entrada.close()
+
+
+archivo_salida = open('numerado.txt', 'w')
+for numero, linea in enumerate(lineas, start=1):
+
+    archivo_salida.write(f"{numero}. {linea}")
+archivo_salida.close()
+
+print("Archivo 'numerado.txt' creado exitosamente.")
+
---
+++ CasadoBayonAntonioJesusFicheros/Ejercicio05/Ejercicio05.py
@@ -0,0 +1,17 @@
+# Realiza un programa que lea el fichero 'datos.txt' y cree un nuevo fichero 'invertido.txt' con las
+#líneas en orden inverso.
+
+
+archivo_entrada = open('datos.txt', 'r')
+lineas = archivo_entrada.readlines()
+archivo_entrada.close()
+
+
+lineas_invertidas = lineas[::-1]
+
+
+archivo_salida = open('invertido.txt', 'w')
+archivo_salida.writelines(lineas_invertidas)
+archivo_salida.close()
+
+
