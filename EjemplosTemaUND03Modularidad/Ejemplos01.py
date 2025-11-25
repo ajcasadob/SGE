c72a1ad5e16c0ebcb8df80561f5e99ec219207ef
ajcasadob <ajcasbay@gmail.com>
2025-11-25 08:57:24 +0100

    done

---
+++ EjemplosTemaUND03Modularidad/Ejemplos01.py
@@ -0,0 +1,23 @@
+def say_hello():
+    print('hello')
+    
+
+say_hello()
+
+def avg(cantidadUno,cantidadDos):
+    return cantidadUno+cantidadDos;
+
+
+print(avg(4,5))
+
+
+def say_bye():
+    return 'Nos vemos loco'
+
+print(say_bye())
+
+def calcularMedia(cantidadUno,cantidad2):
+    return (cantidadUno+cantidad2)/2
+
+
+print(calcularMedia(20,40))
