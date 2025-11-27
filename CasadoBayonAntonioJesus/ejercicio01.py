ddfc02e51b48a039f2c81421bb1ddb3f3fb8983e
ajcasadob <ajcasbay@gmail.com>
2025-11-27 09:09:43 +0100

    done

---
+++ CasadoBayonAntonioJesus/ejercicio01.py
@@ -0,0 +1,30 @@
+#Escriba un programa que pida un número. Después pregunte cuántos números se van a introducir, pida
+#esos números, y escriba cuántos de esos números era mayor que el anterior.
+
+num = int(input('Introduce un numero'))
+
+listaNumeros = [num]
+
+numLista = int(input('Cuantos numeros desea introducir'))
+
+
+
+for i in range (numLista):
+    
+    numIntroducir= int(input('Introduce esos numeros'))
+    listaNumeros.append(numIntroducir)
+
+def lista():
+    contador = 0
+    for i in range (1, len(listaNumeros)):
+        if listaNumeros[i] > listaNumeros[i-1]:
+            contador+=1
+            
+            
+    print(f'La cantidad de numeros mayores es {contador}') 
+
+    
+
+    
+lista()     
+    
