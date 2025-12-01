aa14f39f25fe804e9f3e4db73464e8a21f289b64
ajcasadob <ajcasbay@gmail.com>
2025-12-01 09:01:31 +0100

    done

--- CasadoBayonAntonioJesus/ejercicio02.py
+++ CasadoBayonAntonioJesus/ejercicio02.py
@@ -16,11 +16,23 @@ def ordenar_numeros(num1, num2, num3):
     numeros.sort()
     return numeros
 
-
+def ordenar_manual(numeros):
+    newList= []
+    for i in numeros:
+        maxNum = max(numeros)
+        mixNum = min(numeros)
+        newList.insert(0,mixNum)
+        newList.insert(2, maxNum)
+   
+    
+    
+    return numeros
+    
 
 def mostrar_resultado(lista_ordenada):
     
     print(f"Lista ordenada: {lista_ordenada}")
+    
 
 
 def main():
@@ -28,6 +40,7 @@ def main():
     numero1, numero2, numero3 = solicitar_numeros()
     lista_ordenada = ordenar_numeros(numero1, numero2, numero3)
     mostrar_resultado(lista_ordenada)
+   
 
 
 main()
